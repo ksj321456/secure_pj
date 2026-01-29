d445abe1af797cecb4a28f0cc29d409120466bb3
ksj321456 <ksj321456@gmail.com>
2026-01-29 17:10:59 +0900

    nginx 설정 및 SSL 인증서, 개인키 설치

---
+++ src/main/java/kr/ac/ksj/secure_pj/SecurePjApplication.java
@@ -0,0 +1,14 @@
+package kr.ac.ksj.secure_pj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(
+)
+public class SecurePjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurePjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/ac/ksj/secure_pj/SecurePjApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.ksj.secure_pj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurePjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
